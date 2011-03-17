9a186ccc2a94a2acb65a9debb894442f9682a8cd
Umar <umar@intelligrape.com>
2011-03-18 00:16:09 +0530

    home controller changes

--- grails-app/controllers/com/products/store/HomeController.groovy
+++ grails-app/controllers/com/products/store/HomeController.groovy
@@ -3,7 +3,8 @@ package com.products.store
 class HomeController {
 
     def index = {
-
+       String uuid = UUID.randomUUID().toString();
+              println("uuid = " + uuid);
 //    redirect(action:'index')
 
     }

1c76a181dd173d820cf45cb4740c8b838076de7d
Umar <umar@intelligrape.com>
2011-03-23 23:31:36 +0530

    common js changes

--- web-app/js/common.js
+++ web-app/js/common.js
@@ -3,11 +3,25 @@ jQuery(function(){
         jQuery('#singUp, #newUser').click(function(){
         jQuery('#register').show();
         jQuery('#forgotpassword').hide();
-    })
+    });
     jQuery('#forgot').click(function(){
         jQuery('#register').hide();
         jQuery('#forgotpassword').show();
         return false
     })
-})
-jQuery('.mess')
+});
+jQuery(function(){
+jQuery('a').click(function(event){
+    alert('Thanks for your visiting.');
+    event.preventDefault();
+
+});
+    jQuery('a').addClass('umar');
+    jQuery('a').click(function(event){
+        event.preventDefault();
+        jQuery(this).hide('slow')
+    })
+});
+$.get('home', function(){
+    mycallback(param1,param2);
+});
